885a2fe7e5c4c563649e2bfc5f29c0755f31f336
Hawkjeet <jeetmulani2006@gmail.com>
2026-05-17 04:17:26 +0530

    Password Encryption

--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,9 @@
 const {
   Model
 } = require('sequelize');
+
+const bcrypt = require('bcrypt');
+const { SALT } = require('../config/serverConfig');
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
@@ -20,5 +23,10 @@ module.exports = (sequelize, DataTypes) => {
     sequelize,
     modelName: 'User',
   });
+
+  User.beforeCreate((user) => {
+    const encryptedPassword = bcrypt.hashSync(user.password, SALT);
+    user.password = encryptedPassword;
+  })
   return User;
 };
